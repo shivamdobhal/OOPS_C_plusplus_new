bcee26d44915ff90804f723dacb8bde7a9878ef6
Shiv@m dobhal <73071952+shivamdobhal@users.noreply.github.com>
2023-09-02 14:51:34 +0530

    Update and rename note15--------types of inheritance.cpp to note15--------Types of Inheritance.cpp

--- note15--------Types of Inheritance.cpp
+++ note15--------Types of Inheritance.cpp
@@ -1,7 +1,7 @@
 //single inheritance
 #include <iostream>
 using namespace std;
-
+ 
 class animal{
     public:
      string name;
@@ -164,3 +164,58 @@ output---->barking
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 // 5.hybrid inheritance
+#include <iostream>
+using namespace std;
+
+class animal{              //----------------------->parent1
+
+    public:
+     void func1(){
+               cout<<"barking"<<endl;
+     }
+};
+
+class D{
+    public:
+     void func4(){
+               cout<<"func4"<<endl;
+     }
+};
+
+class dog:public animal{        //------------------------>parent2
+public:
+    void func2(){
+        cout<<"hello i am dog"<<endl;
+    }
+
+
+};
+
+class cat:public animal,public D{               //----------------------->child 
+public: 
+      void func3(){
+        cout<<"hello i am cat"<<endl;
+    }
+};
+
+int main(){
+    cat  obj1;
+    obj1.func1();
+    obj1.func3();
+    obj1.func4();
+    
+    dog  obj2;
+    obj2.func1();
+    obj2.func2();
+
+
+
+    return 0;
+}
+
+// output----------------
+//                     barking
+//                     hello i am cat
+//                     func4
+//                     barking
+//                     hello i am dog
